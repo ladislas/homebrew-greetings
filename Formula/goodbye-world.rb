624c76b7c94502f06f25d3f1080c1d83d9cacb20
Ladislas de Toldi <ladislas@detoldi.me>
2019-06-04 09:58:43 +0200

    remove bottle block

--- Formula/goodbye-world.rb
+++ Formula/goodbye-world.rb
@@ -4,15 +4,6 @@ class GoodbyeWorld < Formula
 
   url "https://github.com/ladislas/goodbye-world/archive/2.0.0.tar.gz"
   sha256 "d90ec02e8966c97a07038bf123e709ea0985db8bb354ab3d798457ad71aee086"
-  revision 1
-
-  bottle do
-    root_url "https://dl.bintray.com/ladislas/bottles-greetings"
-    cellar :any_skip_relocation
-    rebuild 2
-    sha256 "9d77c025fcd3c22570d2149be04caf7dc631caf2f666e4ca68138dab71ba02ff" => :mojave
-    sha256 "7ed581d4938046ff997c4f1eec4a092c95c673dc275cefe800b85d6b5c4aedad" => :high_sierra
-  end
 
   def install
     system "make"
