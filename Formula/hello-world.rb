1bdf1157940fcc262a6f36c4553dbfd3b6c2feb6
Ladislas de Toldi <ladislas@detoldi.me>
2019-06-04 08:08:00 +0200

    add hello-world formula

---
+++ Formula/hello-world.rb
@@ -0,0 +1,16 @@
+class HelloWorld < Formula
+  desc "Simple C++ Hello World CLI to play with Homebrew"
+  homepage "https://github.com/ladislas/hello-world"
+
+  url "https://github.com/ladislas/hello-world/archive/7.0.0.tar.gz"
+  sha256 "c1bc5eb9621fd629cc6972dafe08cc9f345cd4f8755108ed5389b77024222078"
+
+  def install
+    system "make"
+    bin.install "build/hello"
+  end
+
+  test do
+    assert_equal `hello`, "Hello, #{ENV["USER"]}!\nVersion #{version}\n"
+  end
+end
