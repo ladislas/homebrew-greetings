d3aff3a3dcba014ecf88f5138a9646a5f78bb1da
ladislas <ladislas@detoldi.me>
2019-06-04 09:59:25 +0000

    hello-world: add 8.0.0 bottle.

--- Formula/hello-world.rb
+++ Formula/hello-world.rb
@@ -5,6 +5,13 @@ class HelloWorld < Formula
   url "https://github.com/ladislas/hello-world/archive/8.0.0.tar.gz"
   sha256 "0caa1306c4a6c629aed2b66080375c24fc3b820fd803f9e327ae140a30da336d"
 
+  bottle do
+    root_url "https://dl.bintray.com/ladislas/bottles-greetings"
+    cellar :any_skip_relocation
+    sha256 "c09aee0605e24c1503f73607f72d2acdcfd966337883a8f6688faef08680751a" => :mojave
+    sha256 "7800594a2d3f0a0a628362f2c8cf6a7d2238dc65205be2ca1a3e1f65780f1929" => :high_sierra
+  end
+
   def install
     system "make"
     bin.install "build/hello"
